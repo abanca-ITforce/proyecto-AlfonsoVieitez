32674018d44124ce66b569136744dc72fdc9534a
AlfonsoVieitez <57632199+AlfonsoVieitez@users.noreply.github.com>
2019-12-05 19:09:48 +0100

    v5.1

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
+import { FilterFormComponent } from './filter-form/filter-form.component';
 
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, FilterFormComponent],
   imports: [
     CommonModule,
     SearchRoutingModule
